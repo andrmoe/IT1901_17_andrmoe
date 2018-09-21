acc9217e22a84f3beecb65ddc7391d59cb2e830e
andm <andreas.moe@live.com>
2018-09-21 17:10:21 +0200

    Forbedra html

--- pinpoint/view_content/urls.py
+++ pinpoint/view_content/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
 from django.views.generic.list import ListView
-from .models import Post
 
 
 urlpatterns = [
     path('', views.index),
-    path('posts/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:20],
-                              template_name="view_content/TEMPORARY.html"), name="index"),
 
 ]
--- pinpoint/view_content/views.py
+++ pinpoint/view_content/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return HttpResponse("<h1>Hey "+request.user.username+"!</h1>")
+    posts = Post.objects.all().order_by("-date")[:20]
+    return render(request, "view_content/TEMPORARY.html", {'posts': posts, 'user': request.user})
