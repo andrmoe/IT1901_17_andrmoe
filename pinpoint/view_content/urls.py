d70f9fb3ead124344344ed16be034a8fd9c9ba2c
andm <andreas.moe@live.com>
2018-09-21 03:00:39 +0200

    Fullført brukerhistorie 3 uten design

--- pinpoint/view_content/urls.py
+++ pinpoint/view_content/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.list import ListView
+from .models import Post
 
 
 urlpatterns = [
-    path('', views.index, name="index")
-
+    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:20],
+                              template_name="view_content/TEMPORARY.html"), name="index")
 ]
