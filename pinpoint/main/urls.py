a477e91583f1c3e3f3bbdc8113612a0e3858f1b3
aschmirthan <aschmirs@stud.ntnu.no>
2018-09-25 15:45:19 +0200

    endret name

--- pinpoint/main/urls.py
+++ pinpoint/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # register
-    path('', views.register),
-    path('welcome/', views.welcome)
+    path('', views.register, name="register"),
+    path('welcome/', views.welcome, name="home")
 ]
