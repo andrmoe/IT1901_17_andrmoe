0c74b0d0e70b5bf5697b61ad203c482c99d023e9
andm <andreas.moe@live.com>
2018-09-25 14:16:36 +0200

    La til offentlig/privat-post

--- pinpoint/view_content/models.py
+++ pinpoint/view_content/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     body = models.TextField()
     date = models.DateTimeField()
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
