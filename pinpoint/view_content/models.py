656a7b8bad1454684d99789b78a4e9213055c05f
haavbjo <haavbjo@stud.ntnu.no>
2018-10-02 16:21:46 +0200

    Added automatic date to create content

--- pinpoint/view_content/models.py
+++ pinpoint/view_content/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     published = models.BooleanField(default=False)
 
