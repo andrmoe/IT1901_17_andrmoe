dde513c5417a43ba814c60abce7f487bddcfa35c
aschmirthan <aschmirs@stud.ntnu.no>
2018-10-02 15:55:25 +0200

    Added brukerhistorie 4

--- pinpoint/view_content/urls.py
+++ pinpoint/view_content/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.index, name="home"),
     path('create/', views.create_content, name="content"),
+    path('<post_id>', views.detailPost, name='detail'),
+
 ]
