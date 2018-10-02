08164a5f2fef29d700fa367840606d2427476aa5
andm <andreas.moe@live.com>
2018-10-02 22:04:04 +0200

    Utvida textboksen i create_content

--- pinpoint/view_content/forms.py
+++ pinpoint/view_content/forms.py
@@ -1,5 +1,6 @@
 from django.forms.models import modelform_factory
+from django.forms import Textarea
 from .models import Post
 
 
-PostForm = modelform_factory(Post, exclude=['date', 'author'])
+PostForm = modelform_factory(Post, exclude=['date', 'author'], widgets={'body': Textarea(attrs={'cols': 100, 'rows': 100})})
