6c85a4845df200e3f68d6f1f02e81955b0909c9f
andm <andreas.moe@live.com>
2018-10-03 15:14:25 +0200

    Added extra fields to Post

--- pinpoint/view_content/forms.py
+++ pinpoint/view_content/forms.py
@@ -3,4 +3,4 @@ from django.forms import Textarea
 from .models import Post
 
 
-PostForm = modelform_factory(Post, exclude=['date', 'author'], widgets={'body': Textarea(attrs={'cols': 100, 'rows': 100})})
+PostForm = modelform_factory(Post, fields=['title', 'body'], widgets={'body': Textarea(attrs={'cols': 100, 'rows': 100})})
--- pinpoint/view_content/models.py
+++ pinpoint/view_content/models.py
@@ -5,7 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='author_of')
+    editor = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='editor_of', null=True)
+    proof_read = models.BooleanField(default=False)
     published = models.BooleanField(default=False)
 
     def __str__(self):
