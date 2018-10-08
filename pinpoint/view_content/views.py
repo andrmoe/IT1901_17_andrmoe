a82a6cd78d4af875eccae308e7ab8e66e4aad3b7
andm <andreas.moe@live.com>
2018-10-08 15:58:33 +0200

    La til restriksjon hvor du må være editor for å trykke 'assign to self'

--- pinpoint/view_content/views.py
+++ pinpoint/view_content/views.py
@@ -2,6 +2,11 @@ from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm, EditForm
+from django.contrib.auth.models import Group
+
+
+def is_editor(user):
+    return user.groups.filter(name='editor').exists()
 
 
 def index(request):
@@ -43,14 +48,16 @@ def edit_post(request, post_id):
 
 
 def my_page(request):
+    I_am_editor = is_editor(request.user)
     posts = Post.objects.filter(author=request.user)
     needs_proofreading = Post.objects.filter(needs_proofreading=True)
-    return render(request, "view_content/my_page.html", {'posts': posts, 'needs_proofreading': needs_proofreading})
+    return render(request, "view_content/my_page.html",
+                  {'posts': posts, 'needs_proofreading': needs_proofreading, 'is_editor': I_am_editor})
 
 
 def assign_post_editor_to_logged_in_user(request, post_id):
-    # TODO check if user is editor and logged in
-    post = Post.objects.get(id=post_id)
-    post.editor = request.user
-    post.save()
+    if is_editor(request.user):
+        post = Post.objects.get(id=post_id)
+        post.editor = request.user
+        post.save()
     return redirect("/my_page/")
