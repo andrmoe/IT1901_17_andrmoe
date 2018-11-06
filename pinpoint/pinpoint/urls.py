11bdf4c8fa648b37e4a1102f56b865732a8f5158
andm <andreas.moe@live.com>
2018-11-06 15:05:30 +0100

    fully uninstalled select2

--- pinpoint/pinpoint/urls.py
+++ pinpoint/pinpoint/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', include('view_content.urls')),
     path('accounts/', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('select2/', include('django_select2.urls'))
 ]
